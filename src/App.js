7abca27f11f1597d423d09e23bf11507401830ef
Karthi <karthiraja268@gmail.com>
2021-08-15 15:27:09 +0530

    app.js edited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Dashboard from './Dashboard';
 import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 import {
-  BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
@@ -19,30 +18,28 @@ import Editproducts from './Editproducts';
 
 function App() {
   return (
-    <Router>
-      <div className="App wrapper">
-        <Sidebar />
-        <div className="content-wrapper d-flex flex-column">
-          <div className="content">
-            <Topbar />
-            <Switch>
-              <Route path="/" component={Dashboard} exact={true} />
-              <UserProvider>
-                <Route path="/users" component={Users} exact={true} />
-                <Route path="/createuser" component={CreateUser} exact={true} />
-                <Route path="/users/edit/:id" component={Editusers} exact={true} />
-                <ProductProvider>
-                  <Route path="/products" component={Products} exact={true} />
-                  <Route path="/createproducts" component={CreateProducts} exact={true} />
-                  <Route path="/products/edit/:id" component={Editproducts} exact={true} />
-                </ProductProvider>
-              </UserProvider>
 
-            </Switch>
-          </div>
+    <div className="App wrapper">
+      <Sidebar />
+      <div className="content-wrapper d-flex flex-column">
+        <div className="content">
+          <Topbar />
+          <Switch>
+            <Route path="/" component={Dashboard} exact={true} />
+            <UserProvider>
+              <Route path="/users" component={Users} exact={true} />
+              <Route path="/createuser" component={CreateUser} exact={true} />
+              <Route path="/users/edit/:id" component={Editusers} exact={true} />
+              <ProductProvider>
+                <Route path="/products" component={Products} exact={true} />
+                <Route path="/createproducts" component={CreateProducts} exact={true} />
+                <Route path="/products/edit/:id" component={Editproducts} exact={true} />
+              </ProductProvider>
+            </UserProvider>
+          </Switch>
         </div>
       </div>
-    </Router>
+    </div>
   );
 }
 
